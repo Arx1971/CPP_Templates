392768669cc198dcc5f55310dc2526aac438f6ac
Arx1971 <adnan.rahin13@gmail.com>
2020-02-06 08:20:54 -0500

    Stack Implementation

---
+++ Stack_Template/cmake-build-debug/CMakeFiles/Stack_Template.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stack_Template.dir/main.cpp.o"
+  "Stack_Template"
+  "Stack_Template.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stack_Template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
